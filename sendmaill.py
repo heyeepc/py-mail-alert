073d470b2756b8901c51bd571e5b9b9f846da57f
heyeepc <2675458141@qq.com>
2026-05-14 21:37:43 +0800

    更新

--- sendmaill.py
+++ sendmaill.py
@@ -3,3 +3,12 @@ from email.parser import BytesParser
 from email.header import decode_header
 from email.utils import parseaddr
 
+email = ""
+password = ""
+pop3_server = ""
+
+server = smtplib.poplib.POP3_SSL(pop3_server)
+
+server.set_debuglevel(1)
+print(server.getwelcome().decode('utf8'))
+
