8e7a18148ad7d8a7dd15baf28e2e46f8b171f709
heyeepc <2675458141@qq.com>
2026-05-15 23:13:12 +0800

    更新

--- sendmaill.py
+++ sendmaill.py
@@ -12,3 +12,8 @@ server = smtplib.poplib.POP3_SSL(pop3_server)
 server.set_debuglevel(1)
 print(server.getwelcome().decode('utf8'))
 
+server.user(email)
+server.pass_(password)
+
+print("邮件数量 ：s个.大小：%.2fmb"%(server.stat()[0],server.stat()[1]/1024/1024))
+
