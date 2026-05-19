08edd94f83b9f0ea47536bb17b8c4ffef738bba2
heyeepc <2675458141@qq.com>
2026-05-19 23:36:06 +0800

    更新

--- txt2mail.py
+++ txt2mail.py
@@ -2,3 +2,8 @@ import smtplib
 import chardet
 import os
 from email.mime.multipart import MIMEMultipart
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+
+class txtmail(object):
+    def __init__(self):
