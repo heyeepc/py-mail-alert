3ad2f48876e8d18ad781486294fa148f8f434d07
heyeepc <2675458141@qq.com>
2026-05-26 15:11:11 +0800

    更新

--- txt2mail.py
+++ txt2mail.py
@@ -6,4 +6,18 @@ from email.header import Header
 from email.mime.multipart import MIMEMultipart
 
 class txtmail(object):
-    def __init__(self):
+    def __init__(self,host=None,auth_user=None,auth_password=None):
+        self.host = "smtp.qq.com" if host is None else host
+        self.auth_user = auth_user = "xxxxx" if auth_user is None else auth_user
+
+        self.auth_password = "" if auth_password is None else auth_password
+        self.sender = ""
+
+    def send_mail(self,subject,msg_str,receiver_list):
+
+        message = MIMEMultipart()
+        message["From"] = self.sender
+        message["To"] = Header(",".join(receiver_list),"utf-8")
+
+
+
